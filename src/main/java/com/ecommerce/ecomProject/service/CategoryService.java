1ee44cf05d9bca96a12ac9e173c3057e134fc915
ShwetaP143 <rajputshweta152@gmail.com>
2025-08-03 16:59:58 +0530

    New code added

--- src/main/java/com/ecommerce/ecomProject/service/CategoryService.java
+++ src/main/java/com/ecommerce/ecomProject/service/CategoryService.java
@@ -1,4 +1,10 @@
 package com.ecommerce.ecomProject.service;
 
+import com.ecommerce.ecomProject.model.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
 }
